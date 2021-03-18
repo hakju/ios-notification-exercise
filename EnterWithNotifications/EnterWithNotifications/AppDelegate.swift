57ce94bf8dc4b328a2fe212bc0435d0a345ac5d1
HakJu <gkrwnrh@gmail.com>
2021-03-18 15:55:45 +0900

    feat: 학습활동

--- EnterWithNotifications/EnterWithNotifications/AppDelegate.swift
+++ EnterWithNotifications/EnterWithNotifications/AppDelegate.swift
@@ -13,7 +13,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UNUserNotificationCenter.current().getNotificationSettings { setting in
+            if setting.authorizationStatus == .authorized {
+                self.makeNotification()
+            } else {
+                self.requestAuthorization()
+            }
+        }
         return true
+    }
+    
+    func requestAuthorization() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: { didAllow, error in
+            if error == nil && didAllow {
+                self.makeNotification()
+            }
+        })
+    }
+    
+    func makeNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Hi zziro"
+        content.body = "This is the letter from England..."
+        content.userInfo = ["target_view" : "yellow_view"]
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        let request = UNNotificationRequest(identifier: "UN-바다", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
     }
 
     // MARK: UISceneSession Lifecycle
@@ -33,3 +59,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {        
+        if let userInfo = response.notification.request.content.userInfo as? [String : String] {
+            switch userInfo["target_view"] {
+            case "yellow_view":
+                print("dd")
+            default:
+                print("default")
+            }
+        }
+    }
+}
--- EnterWithNotifications/EnterWithNotifications/ViewController.swift
+++ EnterWithNotifications/EnterWithNotifications/ViewController.swift
@@ -7,12 +7,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func touchUpButton(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        makeNotification()
+        
     }
-
-
+    
+//    func makeNotification() {
+//        let content = UNMutableNotificationContent()
+//        content.title = "Hi zziro"
+//        content.body = "This is the letter from England..."
+//        content.userInfo = ["target_view" : "yellow_view"]
+//
+//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+//        let request = UNNotificationRequest(identifier: "UN-바다", content: content, trigger: trigger)
+//        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+//    }
 }
 
